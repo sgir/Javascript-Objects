4f65b322ff1c2dc4254299bf36ec2a469f467e85
Sathya Prabha Girish <sathya.prabha.girish@sap.com>
2016-07-14 22:46:16 -0400

    simple Javascript objects

---
+++ script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var cat = {
+    name: "fluffy",
+    color: "White"
+};
+
+cat.age = 3;
+cat.speak = fuction(){
+    console.log("Meoww")
+}
+
+console.log(cat.name);
+console.log(cat.age);
